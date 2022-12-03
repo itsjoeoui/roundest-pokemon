940660b2cb338e6871dd2957e46f625169af37ff
Joey Yu <joey@itsjoeoui.com>
2022-12-02 22:36:42 -0500

    Basic voting page UI
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="">Hello World</div>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="text-center text-2xl subpixel-antialiased">
+        Which Pokémon is Rounder?
+      </div>
+      <div className="py-2"></div>
+      <div className="flex items-center justify-between rounded-md border-2 p-8">
+        <div className="h-16 w-16 rounded-md border-2 hover:bg-sky-500">
+          First
+        </div>
+        <div className="p-8">VS</div>
+        <div className="h-16 w-16 rounded-md border-2 hover:bg-sky-500">
+          Second
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
